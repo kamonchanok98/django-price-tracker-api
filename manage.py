6723ef71e198fa09893eea45437b120f71ebee4b
kamonchanok <nicelove021141@gmail.com>
2026-08-08 00:09:51 +0700

    scraper backend

---
+++ manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+#!/usr/bin/env python
+import sys
+import re
+import django.utils.cache
+
+# Patch the missing function expected by older DRF
+django.utils.cache.cc_delim_re = re.compile(r'\s*,\s*')
+
+import os
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
